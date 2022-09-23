9a0055fd6eaac8e5be5c8d126f84cef9d077e2e9
Freek Gruntjes <freek@hipex.io>
2022-09-23 13:23:54 +0200

    chore: Code formatting

--- App.Lib/src/Message/BankTransactionImportMessage.cs
+++ App.Lib/src/Message/BankTransactionImportMessage.cs
@@ -5,7 +5,7 @@ namespace App.Lib.Message;
 public class BankTransactionImportMessage
 {
     public IList<BankTransaction> Transactions { get; }
-    
+
     public BankTransactionImportMessage(IList<BankTransaction> transactions)
     {
         Transactions = transactions;
