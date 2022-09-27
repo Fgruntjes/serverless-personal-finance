1bb8b4b9b6355a64f0ab1799ff99818e12e6b9a4
Freek Gruntjes <freek@hipex.io>
2022-09-27 15:06:23 +0200

    chore: Implemented initial test case

--- App.Function.Banktransaction.Import.Tests/src/Controller/AppControllerTest.cs
+++ App.Function.Banktransaction.Import.Tests/src/Controller/AppControllerTest.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Net.Http.Json;
+using App.Lib.Dto;
 using App.TestsLib;
 
 namespace App.Function.Banktransaction.Import.Tests.Controller;
@@ -11,6 +14,19 @@ public class AppControllerTest : IntegrationTestFixture<Program>
     [Fact]
     public async void PostAndWaitForDatabase()
     {
-        
+        var values = new BankTransaction[]
+        {
+            new()
+            {
+                Account = "Some Account",
+                Category = "Clothing",
+                Date = new DateTime(2022, 10, 4),
+                Payee = "SomeStore",
+                Value = 220,
+                CurrencyCode = "EUR",
+            }
+        };
+        using var response = await _client.PostAsJsonAsync("/", values);
+        Assert.Equal(HttpStatusCode.Accepted, response.StatusCode);
     }
 }
