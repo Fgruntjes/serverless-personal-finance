5058c164e64b2521363a940bf258b5ea6c14ad74
Freek Gruntjes <freek@hipex.io>
2023-02-18 11:47:33 +0100

    chore: Fixed src/util/bindPromiseToSignal.ts cancel call scope

--- frontend-web/src/util/bindPromiseToSignal.ts
+++ frontend-web/src/util/bindPromiseToSignal.ts
@@ -1,6 +1,6 @@
 import {CancelablePromise} from "../generated/App.Function.Integration.Ynab";
 
 export function bindPromiseToSignal<T>(resultPromise: CancelablePromise<T>, signal?: AbortSignal): Promise<T> {
-    signal?.addEventListener("abort", resultPromise.cancel);
+    signal?.addEventListener("abort", resultPromise.cancel.bind(resultPromise));
     return resultPromise;
 }
