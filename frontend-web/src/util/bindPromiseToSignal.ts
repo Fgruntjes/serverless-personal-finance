249ae01772ec120c9f1e8730caad6620c11d2069
Freek Gruntjes <freek@hipex.io>
2023-02-18 12:12:24 +0100

    chore: Implemented bindPromiseToSignal test

--- frontend-web/src/util/bindPromiseToSignal.ts
+++ frontend-web/src/util/bindPromiseToSignal.ts
@@ -1,4 +1,7 @@
-import {CancelablePromise} from "../generated/App.Function.Integration.Ynab";
+import {CancelablePromise as FunctionBanktransactionImportPromise} from "../generated/App.Function.Banktransaction.Import";
+import {CancelablePromise as FunctionIntegrationYnabPromise} from "../generated/App.Function.Integration.Ynab";
+
+type CancelablePromise<T> = FunctionIntegrationYnabPromise<T>|FunctionBanktransactionImportPromise<T>;
 
 export function bindPromiseToSignal<T>(resultPromise: CancelablePromise<T>, signal?: AbortSignal): Promise<T> {
     signal?.addEventListener("abort", resultPromise.cancel.bind(resultPromise));
